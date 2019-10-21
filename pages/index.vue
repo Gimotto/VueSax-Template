e14c872b25e9da92968134bae467f0109c742305
Luigi Minauda <luigi.minauda@protonmail.com>
2019-10-21 14:28:45 +0200

    Nuxt + vuesax template

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,5 @@
 <template>
   <div>
-    <div class="con-type-example">
-      <vs-navbar :type="type" v-model="activeItem" class="nabarx">
-
-        <div slot="title">
-          <vs-navbar-title>
-            Type {{type}}
-          </vs-navbar-title>
-        </div>
-
-        <vs-navbar-item index="0" active>
-          <a href="#">Home</a>
-        </vs-navbar-item>
-        <vs-navbar-item index="1">
-          <a href="#">News</a>
-        </vs-navbar-item>
-        <vs-navbar-item index="2">
-          <a href="#">Update</a>
-        </vs-navbar-item>
-      </vs-navbar>
-    </div>
     <div class="container">
       <div>
         <logo />
@@ -30,7 +10,6 @@
         <h2 class="subtitle">
           Nuxt.js + VueSax
         </h2>
-        <vs-progress indeterminate color="primary"></vs-progress>
         <vs-row vs-type="flex" vs-align="center" vs-justify="space-around">
           <vs-col vs-w="1"></vs-col>
           <vs-col vs-w="5">
@@ -45,7 +24,6 @@
           </vs-col>
           <vs-col vs-w="1"></vs-col>
         </vs-row>
-        <vs-progress indeterminate color="success"></vs-progress>
         <vs-row vs-type="flex" vs-align="center" vs-justify="space-around">
           <vs-col vs-w="1"></vs-col>
           <vs-col vs-w="5">
@@ -60,48 +38,7 @@
           </vs-col>
           <vs-col vs-w="1"></vs-col>
         </vs-row>
-        <vs-progress indeterminate color="danger"></vs-progress>
       </div>
-    </div>
-    <div 
-      style="
-        margin: 0 auto;
-        width: 60%;
-        margin-top: -150px;
-        margin-bottom: 150px;
-      ">
-      <vs-collapse accordion >
-        <vs-collapse-item>
-          <div slot="header">
-            Collapse item
-          </div>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque rhoncus eros tortor, non fringilla lectus cursus et. Fusce vel nisi ante. Aliquam sit amet lectus pharetra, luctus mi sed, aliquet felis. Mauris a tortor viverra, ornare tellus in, consectetur leo.
-          <br><br>
-          Etiam nec nunc nec nisl luctus tincidunt efficitur vitae elit. Vestibulum iaculis nibh commodo neque ultrices lobortis. Cras magna massa, pretium vitae mattis varius, pharetra nec massa. Aliquam ac ex enim. Quisque consequat dui libero, vel blandit lorem porttitor sit amet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam sed lobortis nisl, quis eleifend metus.
-        </vs-collapse-item>
-        <vs-collapse-item>
-          <div slot="header">
-            Collapse item 2
-          </div>
-          Nunc auctor et leo vitae suscipit. Nullam aliquet purus scelerisque enim hendrerit tristique. Maecenas tincidunt dui arcu, a aliquet nisl venenatis vitae. Praesent mauris ligula, porta at maximus ac, rutrum vitae sapien. Donec a sapien id erat dapibus dignissim sodales in est. Donec gravida dapibus sapien at sollicitudin. Maecenas iaculis quam ex,
-          <br><br>
-          eu aliquam erat sagittis eget. Suspendisse mollis felis nec ipsum vehicula, at posuere libero viverra. Nam hendrerit dapibus eleifend. Aliquam elit nulla, tincidunt pellentesque enim mollis, consectetur placerat enim. Integer condimentum tristique ante et ullamcorper. Mauris placerat pretium ex. Nam aliquam sed tortor sit amet
-          <br><br>
-          efficitur. Mauris quis faucibus nulla. Pellentesque egestas non ipsum vel maximus.
-        </vs-collapse-item>
-        <vs-collapse-item >
-          <div slot="header">
-            Collapse item 3
-          </div>
-          Suspendisse aliquet condimentum diam, sed aliquam nisl dapibus et. Aliquam euismod ullamcorper dolor eu
-        </vs-collapse-item>
-        <vs-collapse-item>
-          <div slot="header">
-            Collapse item 4
-          </div>
-          Suspendisse aliquet condimentum diam, sed aliquam nisl dapibus et. Aliquam euismod ullamcorper dolor eu imperdiet. Nullam eget odio at magna gravida suscipit sed vestibulum odio. Maecenas porta elit vel lectus molestie, eget aliquam enim feugiat. Vivamus nec faucibus nisl. Nunc venenatis tempus finibus.
-        </vs-collapse-item>
-      </vs-collapse>
     </div>
   </div>
 </template>
--- plugins/vuesax.js
+++ plugins/vuesax.js
@@ -4,3 +4,7 @@ import Vuesax from 'vuesax'
 import 'vuesax/dist/vuesax.css'
 Vue.use(Vuesax)
 import 'material-icons/iconfont/material-icons.css'
+
+export default function(context){
+    console.log(context)
+}
